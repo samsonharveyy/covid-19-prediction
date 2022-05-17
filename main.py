aff354f2e0ffd59382847887cd5fcab5357a699e
samsonharvey <harvey.samson@eee.upd.edu.ph>
2022-05-18 00:45:43 +0800

    added and refactored helper functions for random forest

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from rf_helper import performance_metric, evaluate, plot_data, rf_model
+from rf_helper import performance_metric, evaluate, plot_data, rf_model, feature_selection, split_data
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import TimeSeriesSplit
 import pandas as pd 
-from sklearn.model_selection import train_test_split
 
 
-
+#read database
 data = pd.read_csv('datasets/centralized_database.csv')
 
-#x_data = data[['temp', 'humidity', 'precip', 'windspeed', 'pm2.5', 'o3']]
-#x_data = data[["temp", "feelslike", "humidity", "windspeed", "precip", "pm2.5", "pm10","co","so2","no2","o3"]]
+#get input and output features
+#features = ["temp", "feelslike", "humidity", "windspeed", "precip", "pm2.5", "pm10","co","so2","no2","o3"]
 x_data = data[['humidity', 'pm2.5', 'precip', 'o3', 'temp']]
-
+#x_data = data[['temp', 'humidity', 'precip', 'windspeed', 'pm2.5', 'o3']]
+#x_data = data[features]
 y_data = data.iloc[:,13:].values
 
-
+#transform data
 scaler = MinMaxScaler()
 x_data = scaler.fit_transform(x_data)
 y_data = scaler.fit_transform(y_data)
@@ -24,13 +24,21 @@ x_data = scaler.inverse_transform(x_data)
 y_data = scaler.inverse_transform(y_data)
 
 
-title = "[No Scale] Testing Set - Random Forest (Dropped)"
-rf_x_train, rf_x_test, rf_y_train, rf_y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=0)
-prediction = rf_model(rf_x_train, rf_y_train, rf_x_test, rf_y_test)
+
+#build and run model
+rf_x_train, rf_x_test, rf_y_train, rf_y_test = split_data(x_data, y_data)
+#feature_selection(rf_x_train, rf_y_train, features)
+
+
+title = "Feature Selection - Random Forest"
+
+#testing set
+#prediction = rf_model(rf_x_train, rf_y_train, rf_x_test, rf_y_test)
+#plot_data(range(len(prediction[0])), prediction[0], prediction[1], title)
+#performance_metric(prediction[0], prediction[1], x_data)
+
+#training set
+prediction = rf_model(rf_x_train, rf_y_train, rf_x_train, rf_y_train)
 plot_data(range(len(prediction[0])), prediction[0], prediction[1], title)
-
-
-
-
 performance_metric(prediction[0], prediction[1], x_data)
 
